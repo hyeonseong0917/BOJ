0b77edeb68a06c4603a66bc147a691f01f1e3e1c
hyeonseong0917 <sagiri0917@gmail.com>
2023-04-28 00:10:08 +0900

    20058 code

--- search/20058.cpp
+++ search/20058.cpp
@@ -29,15 +29,17 @@ bool isRange(int y, int x){
 	return (y>=0 && x>=0 && y<p && x<p);
 }
 int maxCnt=0;
+int res=0;
 bool visited[MAX][MAX];
-void dfs(int i, int j, int cnt){
+void dfs(int m, int n){
 	for(int i=0;i<4;++i){
-		int ny=i+dy[i];
-		int nx=j+dx[i];
+		int ny=m+dy[i];
+		int nx=n+dx[i];
 		if(isRange(ny,nx) && A[ny][nx]!=0 && !visited[ny][nx]){
 			visited[ny][nx]=1;
-			maxCnt=max(cnt,maxCnt);
-			dfs(ny,nx,cnt+1);
+			++maxCnt;
+			// cout<<maxCnt<<" "<<m<<" "<<n<<endl;
+			dfs(ny,nx);
 		}
 	}
 }
@@ -46,8 +48,10 @@ void solve(){
 	int y=0,x=0;
 	for(int i=0;i<Q;++i){
 		int cur_step=pow(2,L[i]);
-		
+		y=0, x=0;
+		vector<pair<int,int>> melt;
 		while(1){
+			
 			// cout<<y<<" "<<x<<endl;
 			vector<pair<int,int>> v;
 			int cnt=0;
@@ -67,24 +71,7 @@ void solve(){
 			}
 			//회전 끝
 			//얼음 계산
-			for(int j=y;j<y+cur_step;++j){
-				for(int k=x;k<x+cur_step;++k){
-					int ice_cnt=0;
-					for(int m=0;m<4;++m){
-						int ny=j+dy[m];
-						int nx=k+dx[m];
-						if(isRange(ny,nx)){
-							if(A[ny][nx]>0){
-								++ice_cnt;
-							}
-							
-						}
-					}
-					if(ice_cnt<3){
-						--A[j][k];
-					}
-				}
-			}
+			
 			x+=cur_step;
 			if(x>=p){
 				x=0;
@@ -93,20 +80,40 @@ void solve(){
 			if(y==p){
 				break;
 			}
-			// for(int i=0;i<p;++i){
-			// 	for(int j=0;j<p;++j){
-			// 		cout<<A[i][j];
-			// 	}cout<<endl;
-			// }
-			
-				
 		}
-		
+		for(int j=0;j<p;++j){
+				for(int k=0;k<p;++k){
+					if(A[j][k]==0) continue;
+					int ice_cnt=0;
+					for(int m=0;m<4;++m){
+						int ny=j+dy[m];
+						int nx=k+dx[m];
+						if(isRange(ny,nx)){
+							if(A[ny][nx]>0){
+								++ice_cnt;
+							}
+						}
+					}
+					if(ice_cnt<3){
+						melt.push_back({j,k});
+					}
+				}
+		}
+		for(int j=0;j<melt.size();++j){
+			int my=melt[j].first;
+			int mx=melt[j].second;
+			--A[my][mx];	
+		}
+		// for(int i=0;i<p;++i){
+		// 	for(int j=0;j<p;++j){
+		// 		cout<<A[i][j]<<" ";
+		// 	}cout<<endl;
+		// }cout<<endl;
 	}
 	int sum=0;
 	for(int i=0;i<p;++i){
-				for(int j=0;j<p;++j){
-					sum+=A[i][j];
+		for(int j=0;j<p;++j){
+			sum+=A[i][j];
 		}
 	}
 	cout<<sum<<endl;
@@ -118,13 +125,14 @@ void solve(){
 	}
 	for(int i=0;i<p;++i){
 		for(int j=0;j<p;++j){
-			if(!visited[i][j]){
-				dfs(i,j,0);			
+			if(!visited[i][j] && A[i][j]>0){
+				maxCnt=0;
+				dfs(i,j);		
+				res=max(res,maxCnt);	
 			}
-			
 		}
 	}
-	cout<<maxCnt;
+	cout<<res;
 	
 	// for(int i=0;i<p;++i){
 	// 			for(int j=0;j<p;++j){

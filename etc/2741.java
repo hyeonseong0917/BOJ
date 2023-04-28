99f655bb29cf7a26f3e6976deaf31a4bc0cb53da
hyeonseong0917 <sagiri0917@gmail.com>
2023-04-28 12:20:37 +0900

    2741 code

---
+++ etc/2741.java
@@ -0,0 +1,22 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+public class Main
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        String s="";
+        Scanner sc=new Scanner(System.in);
+        s=sc.next();
+        int num=Integer.parseInt(s);
+        for(int i=1;i<=num;++i){
+            System.out.println(i);
+        }
+        // System.out.print(num);
+    }
+}

08cf3f2970863a809162deb488f391cf69fde20c
hyeonseong0917 <sagiri0917@gmail.com>
2023-05-22 23:46:15 +0900

    [BOJ] 7287: 등록

---
+++ etc/7287.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+	// your code goes here
+	cout<<335<<endl;
+	cout<<"dreamsweet";
+	return 0;
+}

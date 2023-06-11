6ed28a7e54acbead6da398b4b47a55561366d378
hyeonseong0917 <sagiri0917@gmail.com>
2023-06-11 23:22:48 +0900

    [BOJ] 5339: 콜센터

---
+++ etc/5339.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	cout<<"     /~\\"<<endl;
+	cout<<"    ( oo|"<<endl;
+	cout<<"    _\\=/_"<<endl;
+	cout<<"   /  _  \\"<<endl;
+	cout<<"  //|/.\\|\\\\"<<endl;
+	cout<<" ||  \\ /  ||"<<endl;
+	cout<<"============"<<endl;
+	cout<<"|          |"<<endl;
+	cout<<"|          |"<<endl;
+	cout<<"|          |";
+	// cout<<"";
+	return 0;
+}

059c0c14599469b9fa2cc6ed33092f5d5a8c4161
hyeonseong0917 <sagiri0917@gmail.com>
2023-07-01 12:06:43 +0900

    [BOJ] 5338: 마이크로소프트 로고

---
+++ etc/5338.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	cout<<"       _.-;;-._"<<endl;
+	cout<<"'-..-'|   ||   |"<<endl;
+	cout<<"'-..-'|_.-;;-._|"<<endl;
+	cout<<"'-..-'|   ||   |"<<endl;
+	cout<<"'-..-'|_.-''-._|"<<endl;
+	return 0;
+}

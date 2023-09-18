6b2fa0b15c240030211d88278282bfe0374e0b29
hyeonseong0917 <sagiri0917@gmail.com>
2023-09-18 11:19:00 +0900

    [BOJ] 24416: 알고리즘 수업 - 피보나치 수 1

--- dp/2565.cpp
+++ dp/2565.cpp
@@ -5,35 +5,42 @@
 using namespace std;
 
 const int MAX=500+1;
-int dp[MAX];
-int A[MAX];
+
+int f[40+1];
+int fib_cnt=1, fibo_cnt=0;
+// 5 4 3 3 2 2 1
+int fib(int n){
+	if(n==1 || n==2){
+		return 1;
+	}
+	++fib_cnt;
+	return fib(n-1)+fib(n-2);
+}
+
+int fibo(int n){
+	if(n<=2){
+		
+		return 1;
+	}
+	
+	if(f[n]){
+		return f[n];
+	}else{
+		++fibo_cnt;
+		return f[n]=fibo(n-1)+fibo(n-2);
+	}
+
+}
 
 int main() {
 	// your code goes here
-	int N=0;
-	cin>>N;
-	for(int i=0;i<MAX;++i){
-		dp[i]=0;
-		A[i]=0;
+	int n=0;
+	cin>>n;
+	for(int i=0;i<40+1;++i){
+		f[i]=0;
 	}
-	for(int i=0;i<N;++i){
-		int a,b;
-		cin>>a>>b;
-		A[a]=b;
-		dp[a]=1;
-	}
-	int max_length=0; 
-	// dp[x]: x인덱스를 마지막으로 가지는 가장 긴 LIS
-	// x를 기준으로 이전 인덱스를 살펴봤을 때 A[x]보다 더 작은 수가 있다면 dp[x]=dp[x-a]+1
-	for(int i=1;i<MAX;++i){
-		for(int j=1;j<i;++j){
-			if(A[i]>A[j]){
-				dp[i]=max(dp[j]+1,dp[i]);
-			}
-		}
-		max_length=max(max_length,dp[i]);
-	}
-	cout<<N-max_length;
-
+	fib(n);
+	fibo(n);
+	cout<<fib_cnt<<" "<<fibo_cnt;
 	return 0;
 }

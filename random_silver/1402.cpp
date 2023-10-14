41f663c9e6166e7d71dca360b150051d2dd48625
hyeonseong0917 <sagiri0917@gmail.com>
2023-10-14 23:27:15 +0900

    [BOJ] 1402: 아무래도이문제는A번난이도인것같다

---
+++ random_silver/1402.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+#define ll long long
+
+int main() {
+	ios_base :: sync_with_stdio(false); 
+	cin.tie(NULL); 
+	cout.tie(NULL);
+	int t=0;
+	cin>>t;
+	while(t--){
+		int a,b;
+		cin>>a>>b;
+		cout<<"yes"<<"\n";
+	}
+	return 0;
+}

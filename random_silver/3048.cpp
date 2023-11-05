7186f24374b19167556cb5f83efb155e82b54a9d
hyeonseong0917 <sagiri0917@gmail.com>
2023-11-05 13:03:39 +0900

    [BOJ] 3048: 개미

--- random_silver/3048.cpp
+++ random_silver/3048.cpp
@@ -10,56 +10,50 @@ using namespace std;
 #define ll long long
 
 
-
-// 같은 원소가 K개 이하로 들어있는 최장 연속 부분 수열
-int N;
-int K;
-const int MAX=200000+1;
-const int DMAX=100+5;
-int arr[MAX]={0,};
+vector<pair<int,char>> v;
+int N=0;
+bool isRange(int num){
+	return (num>=0 && num<N);
+}
 int main() {
 	
 	// your code goes here
 	ios_base :: sync_with_stdio(false); 
 	cin.tie(NULL); 
 	cout.tie(NULL);
-	cin>>N>>K;
-	for(int i=0;i<N;++i){
-		cin>>arr[i];
+	int fst_size, sed_size;
+	cin>>fst_size>>sed_size;
+	string s1,s2;
+	cin>>s1;
+	cin>>s2;
+	for(int i=0;i<fst_size;++i){
+		v.push_back({1,s1[fst_size-i-1]});
 	}
-
-	int L=0, R=N-1;
-	int ans=1;
-	map<int,int> m;
-
-	while(L<=R){
-		m.clear();
-		int next_val=-1;
-		for(int i=L;i<N;++i){
-			++m[arr[i]];
-			if(m[arr[i]]>K){
-				next_val=arr[i];
-				break;
+	for(int i=0;i<sed_size;++i){
+		v.push_back({-1,s2[i]});
+	}
+	N=fst_size+sed_size;
+	int cnt=0;
+	cin>>cnt;
+	// 방향,char
+	while(cnt--){
+		int i=0;
+		while(i<N-1){
+			if(v[i].first==1 && v[i+1].first==-1){
+				pair<int,char> tmp;
+				tmp=v[i];
+				v[i]=v[i+1];
+				v[i+1]=tmp;
+				i+=2;
 			}else{
-				ans=max(ans,i-L+1);
+				i+=1;
 			}
 		}
-		if(next_val==-1){
-			break;
-		}
-		for(int i=L;i<N;++i){
-			--m[arr[i]];
-			if(arr[i]==next_val){
-				L=i+1;
-				break;;
-			}
-		}
-		// cout<<L<<endl;
-		// L=next_idx+1;
 		
 	}
-	cout<<ans;
-
+	for(int i=0;i<v.size();++i){
+		cout<<v[i].second;
+	}
 
 	return 0;
 }

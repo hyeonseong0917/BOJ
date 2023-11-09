270449e8cb5927cb14a43318f265c89c9b5c4cb3
hyeonseong0917 <sagiri0917@gmail.com>
2023-11-09 15:15:17 +0900

    [BOJ] 1956:  등산

--- dijkstra/1956.cpp
+++ dijkstra/1956.cpp
@@ -11,54 +11,49 @@ using namespace std;
 
 int V,E;
 const int MAX=400+1;
-int INF=122232421;
-vector<pair<int,int>> v[MAX];
+int INF=1222232421;
+vector<vector<pair<int,int>>> v;
+int ans=INF;
+int dist[MAX][MAX]={0};
+
 void Input(){
 	cin>>V>>E;
-	for(int i=0;i<E;++i){
-		int a,b,c;
-		cin>>a>>b>>c;
-		v[a].push_back({b,c});
-	}
-}
-int ans=INF;
-int dist[MAX][MAX];
-priority_queue<pair<int,pair<int,int>>> pq;
-void dijkstra(){
 	for(int i=1;i<=V;++i){
 		for(int j=1;j<=V;++j){
 			dist[i][j]=INF;
 		}
 	}
+	v.resize(V+1);
+	for(int i=0;i<E;++i){
+		int a,b,c;
+		cin>>a>>b>>c;
+		v[a].push_back({b,c});
+		dist[a][b]=c;
+	}
+}
+
+
+void solve(){
+	
 	for(int i=1;i<=V;++i){
-		for(int j=0;j<v[i].size();++j){
-			dist[i][v[i][j].first]=v[i][j].second;
-			pq.push({v[i][j].second*-1,{i,v[i][j].first}} );
+		// i번 정점을 경유하는 경우에 대해
+		for(int j=1;j<=V;++j){
+			for(int k=1;k<=V;++k){
+				if(dist[j][i]!=INF && dist[i][k]!=INF){
+					dist[j][k]=min(dist[j][k],dist[j][i]+dist[i][k]);
+				}
+			}
 		}
 		
 	}
-	while(!pq.empty()){
-		int cur_dist=pq.top().first*-1;
-		int from_edge=pq.top().second.first;
-		int to_edge=pq.top().second.second;
-		pq.pop();
-		for(int i=0;i<v[from_edge].size();++i){
-			int next_edge=v[from_edge][i].first;
-			int next_dist=v[from_edge][i].second;
-			if(dist[from_edge][next_edge]>=cur_dist+next_dist){
-				dist[from_edge][next_edge]=cur_dist+next_dist;
-				pq.push({dist[from_edge][next_edge]*-1,{from_edge,next_edge}});
-			}
-		}
-	}
-}
-void solve(){
-	dijkstra();
 	for(int i=1;i<=V;++i){
-		if(dist[i][i]<ans) ans=min(ans,dist[i][i]);
+		ans=min(ans,dist[i][i]);
 	}
-	cout<<ans;
-	
+	if(ans>=INF){
+		cout<<-1;
+	}else{
+		cout<<ans;
+	}
 }
 
 

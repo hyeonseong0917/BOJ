cea86c719ddb9463743746914d5634f1d00b6a86
hyeonseong0917 <sagiri0917@gmail.com>
2024-02-10 00:02:36 +0900

    [BOJ] 2443: 별 찍기 - 6

--- random_silver/2443.cpp
+++ random_silver/2443.cpp
@@ -10,15 +10,15 @@ int main() {
 	cin.tie(NULL); 
 	cout.tie(NULL);
 	int N;
-	cin >> N;
- 
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < i; j++)
-			cout << ' ';
-		for (int j = 1; j <= (2 * (N - i)) - 1; j++)
-			cout << '*';
-		cout << '\n';
+	cin>>N;
+	for(int i=0;i<N;++i){
+		for(int j=0;j<=(2*N-1)-(i+1);++j){
+			if(j<i){
+				cout<<" ";
+			}else{
+				cout<<"*";
+			}
+		}cout<<endl;
 	}
- 
 	return 0;
 }

6a0266987b39f3f2efd8d0ce7bfcbdaa203171d0
hyeonseong0917 <sagiri0917@gmail.com>
2024-08-10 21:28:09 +0900

    [BOJ] 16112: 5차 전직

---
+++ random_silver/16112.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+using namespace std;
+
+#define ll long long
+
+
+int main() {
+	ios_base :: sync_with_stdio(false); 
+	cin.tie(NULL); 
+	cout.tie(NULL);
+	ll o=1;
+	while(o--){
+		ll n,k;
+		cin>>n>>k;
+		vector<ll> v(n,0);
+		for(ll i=0;i<n;++i){
+			cin>>v[i];
+		}
+		sort(v.begin(),v.end());
+		ll ans=0;
+		for(ll i=0;i<n;++i){
+			ans+=v[i]*min(k,i);
+		}
+		cout<<ans;
+	}
+	
+	return 0;
+}

f41c64520f61670463b8c8d4dfd4eb1af76fc2ed
Hyeonseong0917 <sagiri0917@gmail.com>
2024-10-14 00:15:56 +0900

    [BOJ] 29198: 이번에는 C번이 문자열

--- random_silver/29198.cpp
+++ random_silver/29198.cpp
@@ -24,51 +24,10 @@ int main() {
 			sort(s.begin(),s.end());
 			v.push_back(s);
 		}
-		// 각 문자열은 길이 m, n개의 문자열들 중 k개를 선택해야함
-		// aacx aaab
-		vector<string> ans;
-		vector<bool> visited(n,0);
-		for(ll i=0;i<k;++i){
-			// k개를 선택해서 ans에 넣을거임
-			ll min_num=2023101800;
-			bool flag=0;
-			ll last_idx=-1;
-			for(ll j=0;j<m;++j){
-				// j번째 인덱스의 가장 작은 문자열은?
-				for(ll p=0;p<n;++p){
-					if(visited[p]) continue;
-					ll c=v[p][j]-'A';
-					min_num=min(min_num,c);
-				}
-
-				// 가장 작은 문자열이 여러 개인지?
-				ll cnt=0;
-				ll idx=-1;
-				for(ll p=0;p<n;++p){
-					if(visited[p]) continue;
-					if(v[p][j]-'A'==min_num){
-						idx=p;
-						last_idx=p;
-						++cnt;
-					}
-				}
-				cout<<i<<" "<<j<<" "<<min_num<<endl;
-				if(cnt==1){
-					// cout<<j<<endl;
-					visited[idx]=1;
-					flag=1;
-					ans.push_back(v[idx]);
-					break;
-				}
-			}
-			if(!flag){
-				visited[last_idx]=1;
-				ans.push_back(v[last_idx]);
-			}
-		}
+		sort(v.begin(),v.end());
 		string ans_str="";
 		for(ll i=0;i<k;++i){
-			ans_str+=ans[i];
+			ans_str+=v[i];
 		}
 		sort(ans_str.begin(),ans_str.end());
 		cout<<ans_str;

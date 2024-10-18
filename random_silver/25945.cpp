6ee0fc94b710a8f5340a92a770bc799234306707
Hyeonseong0917 <sagiri0917@gmail.com>
2024-10-19 08:58:05 +0900

    [BOJ] 25945: 컨테이너 재배치

--- random_silver/25945.cpp
+++ random_silver/25945.cpp
@@ -30,15 +30,38 @@ int main() {
 		sort(v.begin(),v.end());
 		ll avg=sum/n;
 		ll mod=sum%n;
+		ll q=0;
 		ll ans=0;
-		ll idx=-1;
+		// cout<<mod<<endl;
 		for(ll i=0;i<n-mod;++i){
-			ans+=avg-v[i];
+			if(v[i]<avg){
+				q+=avg-v[i];
+				ans+=avg-v[i];
+			}else if(v[i]>=avg){
+				ll diff=v[i]-(avg);
+				if(diff<=q){
+					q-=diff;
+				}else if(diff>q){
+					ans+=diff-q;
+					q=0;
+				}
+			}
 		}
-		// cout<<ans<<endl;
-		// for(ll i=n-mod;i<n;++i){
-		// 	ans+=v[i]-(avg+1);
-		// }
+		for(ll i=n-mod;i<n;++i){
+			// avg+1로 만들어야 함
+			if(v[i]<avg+1){
+				q+=avg+1-v[i];
+				ans+=avg+1-v[i];
+			}else if(v[i]>=avg+1){
+				ll diff=v[i]-(avg+1);
+				if(diff<=q){
+					q-=diff;
+				}else if(diff>q){
+					ans+=diff-q;
+					q=0;
+				}
+			}
+		}
 		cout<<ans;
 		
 	}

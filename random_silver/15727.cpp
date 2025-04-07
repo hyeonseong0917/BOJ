6b0ae83428cd21f7e9d19acdd73765989700312a
Hyeonseong0917 <sagiri0917@gmail.com>
2025-04-07 23:57:20 +0900

    [BOJ] 15727: 조별과제를 하려는데 조장이 사라졌다

---
+++ random_silver/15727.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+#include <map>
+#include <deque>
+#include <cmath>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+
+int main() {
+	ios_base :: sync_with_stdio(false); 
+	cin.tie(NULL); 
+	cout.tie(NULL);
+	ll t=1;
+	// cin>>t;
+	while(t--){
+		ll n;
+		cin>>n;
+		if(n%5==0){
+			cout<<n/5;
+		}else{
+			cout<<n/5+1;
+		}
+		
+
+	} 
+	return 0;
+}

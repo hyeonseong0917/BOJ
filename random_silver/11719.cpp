5af92667e7277d4217a7b11307110cd8b8ddbc29
Hyeonseong0917 <sagiri0917@gmail.com>
2025-04-22 23:54:55 +0900

    [BOJ] 11719: 그대로 출력하기 2

---
+++ random_silver/11719.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+#include <map>
+#include <deque>
+#include <cmath>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+
+int main() {
+	ios_base :: sync_with_stdio(false); 
+	cin.tie(NULL); 
+	cout.tie(NULL);
+	ll t=1;
+	// cin>>t;
+	while(t--){
+		string str;
+
+		while (getline(cin, str)) {
+			cout << str << endl;
+		}
+
+	} 
+	return 0;
+}

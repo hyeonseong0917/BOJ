1149c023e765da769d3b2a08b1394510b127c5c1
Hyeonseong0917 <sagiri0917@gmail.com>
2025-06-05 22:56:07 +0900

    [BOJ] 25707: 팔찌 만들기

---
+++ random_silver/25707.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+#include <map>
+#include <deque>
+#include <cmath>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+
+int main() {
+	ios_base :: sync_with_stdio(false); 
+	cin.tie(NULL); 
+	cout.tie(NULL);
+	ll t=1;
+	// cin>>t;
+	while(t--){
+		ll n;
+		cin>>n;
+		vector<ll> v(n,0);
+		for(ll i=0;i<n;++i){
+			cin>>v[i];
+		}
+		sort(v.begin(),v.end());
+		ll ans=v[n-1]-v[0];
+		for(ll i=1;i<n;++i){
+			ans+=v[i]-v[i-1];
+		}
+		cout<<ans;
+	} 
+	return 0;
+}

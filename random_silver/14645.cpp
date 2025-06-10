1781ad230a7433fb849402f06143cd8ee3231b22
Hyeonseong0917 <sagiri0917@gmail.com>
2025-06-10 23:27:29 +0900

    [BOJ] 14645: 와이버스 부릉부릉

---
+++ random_silver/14645.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <map>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+
+int main() {
+	// your code goes here
+	ios_base :: sync_with_stdio(false); 
+	cin.tie(NULL); 
+	cout.tie(NULL);
+	cout<<"비와이"<<'\n';
+	return 0;
+}

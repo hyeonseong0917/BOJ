621e723fa4eb56e6f7ca4bc461f65b5b6676bf75
Hyeonseong0917 <sagiri0917@gmail.com>
2025-06-15 23:15:31 +0900

    [BOJ] 24267: 알고리즘 수업 - 알고리즘의 수행 시간 6

---
+++ random_silver/24267.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+#include <map>
+#include <deque>
+#include <cmath>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+
+int main() {
+	ios_base :: sync_with_stdio(false); 
+	cin.tie(NULL); 
+	cout.tie(NULL);
+	ll t=1;
+	// cin>>t;
+	while(t--){
+		ll n;
+		cin>>n;
+		cout<<(n-2)*(n-1)*n/6<<"\n"<<3;
+	} 
+	return 0;
+}

151144fdbd1795a111db2871d319643cf69e0c53
Hyeonseong0917 <sagiri0917@gmail.com>
2025-07-20 23:38:42 +0900

    [BOJ] 2331: 반복수열

--- random_silver/2331.cpp
+++ random_silver/2331.cpp
@@ -2,64 +2,53 @@
 #include <algorithm>
 #include <vector>
 #include <map>
+#include <cmath>
 using namespace std;
 
 #define ll long long
 
-bool isRange(int y, int x){
-	return (y>=1 && x>=0 && y<=6 && x<=5);
-}
 
-int dy[8]={-2,-2,2,2,-1,1,-1,1};
-int dx[8]={-1,1,-1,1,-2,-2,2,2};
-bool visited[10+1][10+1];
 int main() {
 	ios_base :: sync_with_stdio(false); 
 	cin.tie(NULL); 
 	cout.tie(NULL);
-	string pos="";
-	string init_pos="";
-	bool ans=1;
-	map<string,int> m;
-	for(int i=0;i<37;++i){
-		string s;
-		if(i<36){
-			cin>>s;	
-			if(!m[s]){
-				m[s]=1;
-			}else{
-				ans=0;
-			}
-		}else{
-			s=init_pos;
-		}
-		if(i==0){
-			
-			init_pos=s;
-			pos=s;
-		}else{
-			//pos에서 s갈 수 있는지?
-			bool can_go=0;
-			int y=pos[1]-'0';
-			int x=pos[0]-'A';
-			for(int i=0;i<8;++i){
-				int ny=y+dy[i];
-				int nx=x+dx[i];
-				if(!isRange(ny,nx)) continue;
-				if(ny==(s[1]-'0') && nx==(s[0]-'A')){
-					can_go=1;
+	ll o=1;
+	while(o--){
+		ll a,p;
+		cin>>a>>p;
+		map<ll,ll> m;
+		vector<ll> v;
+		v.push_back(a);
+		ll target_num=-1;
+		++m[a];
+		while(1){
+			ll idx=v.size()-1;
+			string s=to_string(v[idx]);
+			// cout<<s<<endl;
+			ll cur_num=0;
+			for(ll i=0;i<s.size();++i){
+				ll c=s[i]-'0';
+				ll d=1;
+				for(ll j=0;j<p;++j){
+					d*=c;
 				}
+				cur_num+=d;
 			}
-			if(!can_go){
-				ans=0;
+			// cout<<cur_num<<" ";
+			if(m[cur_num]){
+				target_num=cur_num;
+				break;
+			}else{
+				v.push_back(cur_num);
+				++m[cur_num];
 			}
-			pos=s;
 		}
-	}
-	if(!ans){
-		cout<<"Invalid";
-	}else{
-		cout<<"Valid";
+		ll ans=0;
+		for(ll i=0;i<v.size();++i){
+			if(v[i]==target_num) break;
+			++ans;
+		}
+		cout<<ans;
 	}
 	return 0;
 }

71e40b18700591cbe36b29567b27e3d1cf255392
Hyeonseong0917 <sagiri0917@gmail.com>
2025-08-26 15:19:31 +0900

    [BOJ] 2179: 비슷한 단어

--- gold/2179.cpp
+++ gold/2179.cpp
@@ -5,33 +5,56 @@
 #include <queue>
 using namespace std;
 
-int N;
-vector<string> v;
-map<string,int> m;
-void Input(){
-	cin>>N;
-	for(int i=0;i<N;++i){
-		string s;
-		cin>>s;
-		m[s]=1;
-		if(!m[s]){
-			m[s]=1;
-			v.push_back(s);	
-		}
-		
-	}
-}
-void solve(){
-	
-}
+#define ll long long
 
 int main() {
 	// your code goes here
 	ios_base :: sync_with_stdio(false); 
 	cin.tie(NULL); 
 	cout.tie(NULL);
-	Input();
-	solve();
+	ll o=1;
+	while(o--){
+		ll n;
+		cin>>n;
+		vector<string> v;
+		map<string,ll> check;
+		for(ll i=0;i<n;++i){
+			string s;
+			cin>>s;
+			if(check[s]) continue;
+			++check[s];
+			v.push_back(s);
+		}
+		map<string,ll> m;
+		ll ans=0;
+		ll L=0,R=0;
+		for(ll i=0;i<v.size();++i){
+			string s=v[i];
+			for(ll j=0;j<s.size();++j){
+				string c=s.substr(0,j+1);
+				if(!m[c]){
+					m[c]=i+1;
+					continue;
+				}
+				ll cs=c.size();
+				if(ans<cs){
+					L=m[c];
+					R=i+1;
+					ans=cs;
+				}else{
+					if(ans==cs){
+						if(L>m[c]){
+							L=m[c];
+							R=i+1;
+						}
+					}
+				}
+			}
+		}
+		// cout<<ans;
+		cout<<v[L-1]<<"\n";
+		cout<<v[R-1];
+	}
 	
 	return 0;
 }

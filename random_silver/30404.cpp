dcb7bfcb283583b295d530cfb4d49020df8c681a
Hyeonseong0917 <sagiri0917@gmail.com>
2025-10-18 12:48:56 +0900

    [BOJ] 30404: 오리와 박수치는 춘배

--- random_silver/30404.cpp
+++ random_silver/30404.cpp
@@ -15,7 +15,21 @@ int main() {
 	cout.tie(NULL);
 	ll o=1;
 	while(o--){
-		
+		ll n,k;
+		cin>>n>>k;
+		vector<ll> v(n,0);
+		for(ll i=0;i<n;++i){
+			cin>>v[i];
+		}
+		ll ans=0;
+		ll cur_time=0;
+		for(ll i=0;i<n;++i){
+			// v[i]+k번째 초에 박수침
+			if(v[i]<=cur_time) continue;
+			++ans;
+			cur_time=v[i]+k;
+		}
+		cout<<ans;
 	}
 	return 0;
 }

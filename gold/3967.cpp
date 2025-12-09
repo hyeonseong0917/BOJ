f89e9f6a49db9ab9d9ed58f34f29394532176b75
Hyeonseong0917 <sagiri0917@gmail.com>
2025-12-10 01:41:07 +0900

    [BOJ] 3967: 매직스타

--- gold/3967.cpp
+++ gold/3967.cpp
@@ -9,161 +9,140 @@ using namespace std;
 
 #define ll long long
 
-string board[5+1];
-bool get_sum(string board[5+1]){
-	// 정삼각형
-	ll y=0, x=4;
-	ll sum=0;
-	bool flag=0;
-	// 왼쪽 대각선 변
-	for(ll i=0;i<4;++i){
-		sum+=board[y+i][x-i]-'A';
+vector<ll> check(12,0);
+string board[5];
+string tmp_board[5];
+vector<string> v;
+vector<pair<ll,ll>> p[6];
+vector<pair<ll,ll>> star_pos;
+bool isStar(string tmp_board[5]){
+	// 합이 22
+	bool flag=1;
+	for(ll i=0;i<6;++i){
+		// i번째 조건 만족?
+		ll sum=0;
+		for(ll j=0;j<4;++j){
+			sum+=tmp_board[p[i][j].first][p[i][j].second]-'A';
+		}
+		if(sum!=22){
+			flag=0;
+			break;
+		}
 	}
-	// cout<<sum<<endl;
-	if(sum!=22){
-		flag=1;
-	}
-	// 중앙 변
-	y=3, x=1, sum=0;
-	for(ll i=1;i<=7;i+=2){
-		sum+=board[y][i]-'A';
-	}
-	// cout<<sum<<endl;
-	if(sum!=22){
-		flag=1;
-	}
-
-	// 오른쪽 대각선 변
-	y=0, x=4, sum=0;
-	for(ll i=0;i<4;++i){
-		sum+=board[y+i][x+i]-'A';
-	}
-	// cout<<sum<<endl;
-	if(sum!=22){
-		flag=1;
-	}
-
-
-	// 역삼각형
-	
-	// 왼쪽 대각선 변
-	y=1, x=1, sum=0;
-	for(ll i=0;i<4;++i){
-		sum+=board[y+i][x+i]-'A';
-	}
-	// cout<<sum<<endl;
-	if(sum!=22){
-		flag=1;
-	}
-	// 오른쪽 대각선 변
-	y=1, x=7, sum=0;
-	for(ll i=0;i<4;++i){
-		sum+=board[y+i][x-i]-'A';
-	}
-	// cout<<sum<<endl;
-	if(sum!=22){
-		flag=1;
-	}
-
-	// 중앙 변
-	y=1, x=1, sum=0;
-	for(ll i=1;i<=7;i+=2){
-		sum+=board[y][i]-'A';
-	}
-	// cout<<sum<<endl;
-	if(sum!=22){
-		flag=1;
-	}
-	if(!flag){
-		return 1;
-	}else{
-		return 0;
-	}
+	return flag;
 }
-bool visited[12+1];
-vector<pair<ll,pair<ll,ll>>> v;
-vector<string> ans;
-bool flag=0;
-map<string,ll> m;
-void pick(ll cy, ll cx){
-	// cout<<cy<<" "<<cx<<endl;
-	if(v.size()==12){
-		string tmp_board[5+1];
-		for(ll i=0;i<5;++i){
-			tmp_board[i]=board[i];
+vector<ll> c;
+void pick(ll idx, ll pos_idx){
+	if(!v.empty()){
+		return;
+	}
+	if(idx==star_pos.size()){
+		ll c_idx=0;
+		for(ll i=0;i<star_pos.size();++i){
+			ll y=star_pos[i].first;
+			ll x=star_pos[i].second;
+			board[y][x]='A'+c[c_idx++];
 		}
-		for(ll i=0;i<v.size();++i){
-			ll y=v[i].second.first;
-			ll x=v[i].second.second;
-			ll c=v[i].first;
-			tmp_board[y][x]='A'+c;
-		}
-		bool cur_sum=get_sum(tmp_board);
-		if(cur_sum){
+		if(isStar(board)){
 			string tmp="";
 			for(ll i=0;i<5;++i){
-				for(ll j=0;j<9;++j){
-					tmp+=tmp_board[i][j];
-				}
+				tmp+=board[i];
 			}
-			ans.push_back(tmp);
+			v.push_back(tmp);
 		}
 		return;
 	}
-	while(1){
-		if(cx==9){
-			cx=0;
-			++cy;
-		}
-		if(cy==5){
-			break;
-		}
-		if(board[cy][cx]!='x'){
-			++cx;
-			continue;
-		}
-		if(board[cy][cx]=='x'){
-			for(ll k=0;k<12;++k){
-				if(!visited[k]){
-					visited[k]=1;
-					v.push_back({k,{cy,cx}});
-					board[cy][cx]='A'+k;
-					pick(cy,cx+1);
-					v.pop_back();
-					visited[k]=0;
-					board[cy][cx]='x';
-				}
-			}
-		}
+	for(ll i=0;i<12;++i){
+		if(!check[i]){
+			check[i]=1;
+			c.push_back(i);
+			ll y=star_pos[pos_idx].first;
+			ll x=star_pos[pos_idx].second;
+			board[y][x]='A'+c[idx];
 
+			pick(idx+1,pos_idx+1);
+
+			c.pop_back();
+			check[i]=0;
+			board[y][x]='x';
+		}
 	}
-	
 }
-string u[5+1];
+
 int main() {
 	ios_base :: sync_with_stdio(false); 
 	cin.tie(NULL); 
 	cout.tie(NULL);
 	ll o=1;
 	while(o--){
-		// abcdefghijkl 1에서 12까지
 		for(ll i=0;i<5;++i){
 			cin>>board[i];
 			for(ll j=0;j<9;++j){
-				if(board[i][j]!='.' && board[i][j]!='x'){
-					++visited[(board[i][j]-'A')];
-					v.push_back({board[i][j]-'A',{i,j}});
+				if(board[i][j]>='A' && board[i][j]<='L'){
+					ll c=board[i][j]-'A';
+					++check[c];
+				}
+				if(board[i][j]=='x'){
+					star_pos.push_back({i,j});
 				}
 			}
+			tmp_board[i]=board[i];
 		}
+		// 정삼각형좌변
+		p[0].push_back({0,4});
+		p[0].push_back({1,3});
+		p[0].push_back({2,2});
+		p[0].push_back({3,1});
+
+		// 정삼각형 밑변
+		p[1].push_back({3,1});
+		p[1].push_back({3,3});
+		p[1].push_back({3,5});
+		p[1].push_back({3,7});
+
+		// 정삼각형 우변
+		p[2].push_back({0,4});
+		p[2].push_back({1,5});
+		p[2].push_back({2,6});
+		p[2].push_back({3,7});
+
+		// 역삼각형 밑변
+		p[3].push_back({1,1});
+		p[3].push_back({1,3});
+		p[3].push_back({1,5});
+		p[3].push_back({1,7});
+
+		// 역삼각형 좌변
+		p[4].push_back({1,1});
+		p[4].push_back({2,2});
+		p[4].push_back({3,3});
+		p[4].push_back({4,4});
+
+		// 역삼각형 우변
+		p[5].push_back({4,4});
+		p[5].push_back({3,5});
+		p[5].push_back({2,6});
+		p[5].push_back({1,7});
+		
+
 		pick(0,0);
-		ll as=ans.size();
-		cout<<as;
-		// for(ll i=0;i<5;++i){
-		// 	cin>>u[i];
-		// }
-		// cout<<get_sum(u);
-		// 합이 26
+		if(!v.empty()){
+			sort(v.begin(),v.end());
+			string k=v[0];
+			vector<string> k_tmp;
+			string k_tmp_str="";
+			for(ll i=0;i<k.size();++i){
+				k_tmp_str+=k[i];
+				ll ks=k_tmp_str.size();
+				if(ks==9){
+					k_tmp.push_back(k_tmp_str);
+					k_tmp_str="";
+				}
+			}
+			for(ll i=0;i<5;++i){
+				cout<<k_tmp[i]<<"\n";
+			}
+		}
 	}
 	return 0;
 }

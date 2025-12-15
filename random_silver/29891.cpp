f89e8fee8d0c3047080229925888351212f819d3
Hyeonseong0917 <sagiri0917@gmail.com>
2025-12-16 01:36:56 +0900

    [BOJ] 29891: 체크포인트 달리기

--- random_silver/29891.cpp
+++ random_silver/29891.cpp
@@ -14,37 +14,36 @@ int main() {
 	cout.tie(NULL);
 	ll o=1;	
 	while(o--){
+		// 0129
 		ll n,k;
 		cin>>n>>k;
 		vector<ll> fst_v, sed_v;
 		for(ll i=0;i<n;++i){
 			ll a;
 			cin>>a;
-			if(a<0){
-				fst_v.push_back(a*-1);
+			if(a==0) continue;
+			if(a>0){
+				fst_v.push_back(a);
 			}else{
-				sed_v.push_back(a);
+				sed_v.push_back(a*-1);
 			}
 		}
 		sort(fst_v.rbegin(),fst_v.rend());
 		sort(sed_v.rbegin(),sed_v.rend());
 		ll ans=0;
-		ll idx=0;
-		ll cnt=0;
-		while(idx<fst_v.size()){
-			if(cnt%k==0){
-				ans+=fst_v[idx]*2;
-			}
-			++idx;
-			++cnt;
+		ll fst_idx=0;
+		ll fs=fst_v.size();
+		while(fst_idx<fs){
+			ll cnt=k;
+			ans+=fst_v[fst_idx]*2;
+			fst_idx+=k;
 		}
-		cnt=0;
-		while(idx<sed_v.size()){
-			if(cnt%k==0){
-				ans+=sed_v[idx]*2;
-			}
-			++idx;
-			++cnt;
+		ll sed_idx=0;
+		ll ss=sed_v.size();
+		while(sed_idx<ss){
+			ll cnt=k;
+			ans+=sed_v[sed_idx]*2;
+			sed_idx+=k;
 		}
 		cout<<ans;
 	} 

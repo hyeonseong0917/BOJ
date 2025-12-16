cdae8f3793105113aff58a596e83ecba1547842f
Hyeonseong0917 <sagiri0917@gmail.com>
2025-12-16 13:43:41 +0900

    [BOJ] 14462: 소가 길을 건너간 이유 8

--- gold/14462.cpp
+++ gold/14462.cpp
@@ -16,8 +16,48 @@ int main() {
 	cout.tie(NULL);
 	ll o=1;
 	while(o--){
+		// 1328
 		// n종류의 소가 있음
-		// 
+		// 1번종,2번종,3번종.. n번종까지 있음
+		ll n;
+		cin>>n;
+		vector<ll> x(n,0), y(n,0);
+		for(ll i=0;i<n;++i){
+			cin>>x[i];
+		}
+		for(ll i=0;i<n;++i){
+			cin>>y[i];
+		}
+		ll dp[n][n];
+		for(ll i=0;i<n;++i){
+			for(ll j=0;j<n;++j){
+				dp[i][j]=0;
+			}
+		}
+		for(ll i=0;i<n;++i){
+			if((ll)abs(x[0]-y[i])<=4){
+				dp[0][i]=1;
+			}
+			if((ll)abs(x[i]-y[0])<=4){
+				dp[i][0]=1;
+			}
+		}
+		for(ll i=1;i<n;++i){
+			for(ll j=1;j<n;++j){
+				dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+				if((ll)abs(x[i]-y[j])<=4){
+					dp[i][j]=max(dp[i][j],dp[i-1][j-1]+1);
+				}
+			}
+		}
+		cout<<dp[n-1][n-1];
+		// ll ans=0;
+		// for(ll i=0;i<n;++i){
+		// 	for(ll j=0;j<n;++j){
+		// 		ans=max(ans,dp[i][j]);
+		// 	}
+		// }
+		// cout<<ans;
 	}
 	return 0;
 }

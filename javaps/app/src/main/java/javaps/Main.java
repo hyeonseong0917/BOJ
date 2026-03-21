c8cff82e3331ac6ebd15b98be0bb970a333ab72f
hyeonseong <sagiri0917@gmail.com>
2026-03-21 15:10:18 +0900

    [BOJ] 15671: 오델로

--- javaps/app/src/main/java/javaps/Main.java
+++ javaps/app/src/main/java/javaps/Main.java
@@ -6,48 +6,8 @@ public class Main {
     static Scanner sc=new Scanner(System.in);
     static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    static int n,m,p,HP;
-    static String board[]=new String[1000+1];
-    static class Player{
-        char id;
-        int dps;
-        public Player(char id, int dps){
-            this.id=id;
-            this.dps=dps;
-        }
-
-    }
-    static ArrayList<Player> v=new ArrayList<>();
-    public static void Input() throws IOException{
-        st = new StringTokenizer(br.readLine());
-        n=Integer.parseInt(st.nextToken());
-        m=Integer.parseInt(st.nextToken());
-        p=Integer.parseInt(st.nextToken());
-        for(int i=0;i<n;++i){
-            st = new StringTokenizer(br.readLine());
-            board[i]=st.nextToken();
-        }
-        for(int i=0;i<p;++i){
-            st = new StringTokenizer(br.readLine());
-            char cur_id=st.nextToken().charAt(0);
-            int cur_dps=Integer.parseInt(st.nextToken());
-            v.add(new Player(cur_id,cur_dps));
-        }
-        st = new StringTokenizer(br.readLine());
-        HP=Integer.parseInt(st.nextToken());
-    }
-    // static class CntNum implements Comparable<CntNum>{
-    //     int cnt;
-    //     int num;
-    //     public CntNum(int cnt, int num){
-    //         this.cnt=cnt;
-    //         this.num=num;
-    //     }
-    //     @Override
-    //     public int compareTo(CntNum other){
-    //         return this.cnt-other.cnt;
-    //     }
-    // }
+    static int n;
+    static char board[][]=new char[6][6];
     static class Pos{
         int y;
         int x;
@@ -56,99 +16,92 @@ public class Main {
             this.x=x;
         }
     }
-    static class DistPos implements Comparable<DistPos>{
-        int dist;
-        int y;
-        int x;
-        public DistPos(int dist, int y, int x){
-            this.dist=dist;
-            this.y=y;
-            this.x=x;
+    static ArrayList<Pos> v=new ArrayList<>();
+    public static void Input() throws IOException{
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<6;++i){
+            for(int j=0;j<6;++j){
+                board[i][j]='.';
+            }
         }
-        @Override
-        public int compareTo(DistPos other){
-            return this.dist-other.dist;
+        board[2][2]='W';
+        board[2][3]='B';
+        board[3][2]='B';
+        board[3][3]='W';
+        n=Integer.parseInt(st.nextToken());
+        for(int i=0;i<n;++i){
+            st = new StringTokenizer(br.readLine());
+            int y=Integer.parseInt(st.nextToken());
+            int x=Integer.parseInt(st.nextToken());
+            v.add(new Pos(y,x));
         }
+
     }
-    static Pos[] init_pos=new Pos[26];
-    static ArrayList<ArrayList<Integer>> time_player=new ArrayList<ArrayList<Integer>>();
-    static int[][] dist=new int[1000+1][1000+1];
-    static int dy[]=new int[]{-1,0,1,0};
-    static int dx[]=new int[]{0,1,0,-1};
+    static int dy[]={-1,-1,-1,0,1,1,1,0};
+    static int dx[]={-1,0,1,1,1,0,-1,-1};
     static boolean isRange(int y, int x){
-        return (y>=0 && x>=0 && y<n && x<m);
+        return (y>=0 && x>=0 && y<6 && x<6);
     }
     static void Solve(){
-        // 1. 각 player별로 도달하기까지 최소 시간이 몇초인지 구한다
-        // 2. 시간=1부터 쭉 순회함. 보스몬스터 체력 0 될때까지.
-        // 3. 데미지 준 player cnt 구함
-        
-        // 0. id에 맞는 좌표를 구하기
-        int by=0,bx=0;
         for(int i=0;i<n;++i){
-            for(int j=0;j<m;++j){
-                dist[i][j]=2023101800;
-                if(board[i].charAt(j)=='B'){
-                    by=i;
-                    bx=j;
-                    continue;
-                }
-                if(board[i].charAt(j)=='.' || board[i].charAt(j)=='B' || board[i].charAt(j)=='X') continue;
-                int a=board[i].charAt(j)-'a';
-                init_pos[a]=new Pos(i,j);
-                
+            int y=v.get(i).y;
+            int x=v.get(i).x;
+            --y;
+            --x;
+            char c='B';
+            if(i%2==1){
+                // 흰돌
+                c='W';
             }
-        }
-        // 1. x시간에 추가되는 플레이어들의 인덱스 넣기
-        for(int i=0;i<=1000000;++i){
-            ArrayList<Integer> arr=new ArrayList<>();
-            time_player.add(arr);
-        }
-        // 2. B의 좌표에서 모든 지점까지의 최소 거리를 구한다
-        dist[by][bx]=0;
-        PriorityQueue<DistPos> pq=new PriorityQueue<>();
-        pq.add(new DistPos(0,by,bx));
-        while(!pq.isEmpty()){
-            DistPos cur_dist_pos=pq.poll();
-            int cur_dist=cur_dist_pos.dist;
-            int y=cur_dist_pos.y;
-            int x=cur_dist_pos.x;
-            // System.out.println(y+" "+x);
-            for(int i=0;i<4;++i){
-                int ny=y+dy[i];
-                int nx=x+dx[i];
-                if(!isRange(ny,nx)) continue;
-                if(board[ny].charAt(nx)=='X') continue;
-                if(dist[ny][nx]>dist[y][x]+1){
-                    dist[ny][nx]=dist[y][x]+1;
-                    pq.add(new DistPos(dist[ny][nx],ny,nx));
+            board[y][x]=c;
+            for(int j=0;j<8;++j){
+                // j번째 방향을 선택함
+                int ny=y+dy[j];
+                int nx=x+dx[j];
+                int flag=0;
+                while(true){
+                    if(!isRange(ny,nx)) break;
+                    if(board[ny][nx]=='.') break;
+                    if(board[ny][nx]==c){
+                        flag=1;
+                        break;
+                    }
+                    ny+=dy[j];
+                    nx+=dx[j];
+                }
+                if(flag==1){
+                    int cy=y;
+                    int cx=x;
+                    while(true){
+                        if(cy==ny && cx==nx) break;
+                        board[cy][cx]=c;
+                        cy+=dy[j];
+                        cx+=dx[j];
+                    }
                 }
             }
+            // for(int j=0;j<6;++j){
+            //     for(int k=0;k<6;++k){
+            //         System.out.print(board[j][k]);
+            //     }System.out.println();
+            // }
         }
-        
-        for(int i=0;i<p;++i){
-            char cur_id=v.get(i).id;
-            int cur_dps=v.get(i).dps;
-            int cur_y=init_pos[cur_id-'a'].y;
-            int cur_x=init_pos[cur_id-'a'].x;
-            // cur_y에서 cur_x까지 얼마나 걸리는지?
-            int cur_time=dist[cur_y][cur_x];
-            if(cur_time==2023101800) continue;
-            time_player.get(cur_time).add(i);
+        int b_cnt=0, w_cnt=0;
+        for(int i=0;i<6;++i){
+            for(int j=0;j<6;++j){
+                System.out.print(board[i][j]);
+                if(board[i][j]=='B'){
+                    ++b_cnt;
+                }else if(board[i][j]=='W'){
+                    ++w_cnt;
+                }
+            }System.out.println();
         }
-        int total_dps=0;
-        int cnt=0;
-        for(int i=1;i<=1000001;++i){
-            // 현재 i초임.
-            HP-=total_dps;
-            if(HP<=0) break;
-            for(int j=0;j<time_player.get(i).size();++j){
-                int cur_idx=time_player.get(i).get(j);
-                total_dps+=v.get(cur_idx).dps;
-                ++cnt;
-            }
+        if(b_cnt<w_cnt){
+            System.out.print("White");
+        }else{
+            System.out.print("Black");
         }
-        System.out.println(cnt);
     }
 
     public static void main(String[] args) throws IOException{

fe0fb520e82754ba1e6f576e4e3a458ed26cbb38
hyeonseong <sagiri0917@gmail.com>
2026-03-31 22:13:37 +0900

    [BOJ] 2917: 늑대 사냥꾼

--- javaps/app/src/main/java/javaps/Main.java
+++ javaps/app/src/main/java/javaps/Main.java
@@ -6,119 +6,137 @@ public class Main {
     static Scanner sc=new Scanner(System.in);
     static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
+    
+    // 이동하는 모든 칸에서 나무와 거리의 최솟값이 가장 크도록 하고싶음
     static int N,M;
-    static ArrayList<ArrayList<Integer>> v=new ArrayList<ArrayList<Integer>>();
-    static ArrayList<ArrayList<Integer>> rv=new ArrayList<ArrayList<Integer>>();
-    static int dist[][]=new int[1000+1][1000+1];
-    static int dp[][]=new int[1000+1][1000+1];
-    static int check[]=new int[1000+1];
-    static ArrayList<Integer> arr=new ArrayList<>();
+    static char board[][]=new char[500+1][500+1];
+    static int dist[][]=new int[500+1][500+1];
     public static void Input() throws IOException{
         st = new StringTokenizer(br.readLine());
         N=Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine());
         M=Integer.parseInt(st.nextToken());
-        for(int i=0;i<=N;++i){
-            ArrayList<Integer> tmp=new ArrayList<>();
-            v.add(tmp);
-            rv.add(new ArrayList<Integer>());
-            for(int j=0;j<=N;++j){
-                dp[i][j]=-1;
-            }
-        }
-        for(int i=0;i<M;++i){
+        for(int i=0;i<N;++i){
             st = new StringTokenizer(br.readLine());
-            int a,b,c;
-            a=Integer.parseInt(st.nextToken());
-            b=Integer.parseInt(st.nextToken());
-            if(a!=1){
-                ++check[b];
+            String str=st.nextToken();
+            for(int j=0;j<M;++j){
+                board[i][j]=str.charAt(j);
             }
-            c=Integer.parseInt(st.nextToken());
-            if(b==1){
-                arr.add(a);
-            }
-            dist[a][b]=c;
-            v.get(a).add(b);
-            rv.get(b).add(a);
         }
     }
-    
+    static class Pos{
+        int y;
+        int x;
+        public Pos(int y, int x){
+            this.y=y;
+            this.x=x;
+        }
+    }
+    static int dy[]={-1,0,1,0};
+    static int dx[]={0,1,0,-1};
+    static boolean isRange(int y, int x){
+        return (y>=0 && x>=0 && y<N && x<M);
+    }
     public static void Solve(){
-        Queue<Integer> q=new LinkedList<>();
-        dp[1][1]=0;
-        for(int i=2;i<=N;++i){
-            if(check[i]==0){
-                q.add(i);
-                dp[i][2]=dist[1][i];
+        Queue<Pos> q=new LinkedList<>();
+        for(int i=0;i<N;++i){
+            for(int j=0;j<M;++j){
+                if(board[i][j]=='+'){
+                    dist[i][j]=0;
+                    q.add(new Pos(i,j));
+                }else{
+                    dist[i][j]=2023101800;
+                }
             }
         }
-        
         while(!q.isEmpty()){
-            int cur_num=q.poll();
-            for(int i=0;i<v.get(cur_num).size();++i){
-                int next_num=v.get(cur_num).get(i);
-                if(next_num==1) continue;
-                --check[next_num];
-                for(int j=2;j<N;++j){
-                    if(dp[cur_num][j]==-1) continue;
-                    if(dp[next_num][j+1]<dp[cur_num][j]+dist[cur_num][next_num]){
-                        dp[next_num][j+1]=dp[cur_num][j]+dist[cur_num][next_num];
-                    }
-                }
-                if(check[next_num]==0){
-                    q.add(next_num);
-                }
-                
-            }
-        }
-        int ans=0;
-        for(int i=0;i<arr.size();++i){
-            int last_num=arr.get(i);
-            for(int j=2;j<=N;++j){
-                if(dp[last_num][j]==-1) continue;
-                ans=Math.max(ans,dp[last_num][j]+dist[last_num][1]);
-            }
-        }
-        System.out.println(ans);
-        ArrayList<Integer> ans_vec=new ArrayList<>();
-        int ans_cnt=0;
-
-        for(int i=0;i<arr.size();++i){
-            if(!ans_vec.isEmpty()) break;
-            int last_num=arr.get(i);
-            for(int j=2;j<=N;++j){
-                if(!ans_vec.isEmpty()) break;
-                if(dp[last_num][j]==-1) continue;
-                if(ans==dp[last_num][j]+dist[last_num][1]){
-                    ans_vec.add(1);
-                    ans_cnt=j;
-                    ans_vec.add(last_num);
-                    ans-=dist[last_num][1];
+            Pos p=q.poll();
+            int y=p.y;
+            int x=p.x;
+            for(int i=0;i<4;++i){
+                int ny=y+dy[i];
+                int nx=x+dx[i];
+                if(!isRange(ny,nx)) continue;
+                if(board[ny][nx]=='+') continue;
+                if(dist[ny][nx]>dist[y][x]+1){
+                    dist[ny][nx]=dist[y][x]+1;
+                    q.add(new Pos(ny,nx));
                 }
             }
         }
-        // System.out.println(ans+" "+ans_cnt);
-        // // ans_vec에는 1,last_num들어있음
-        // 현재 ans_cnt
-        while(true){
-            int as=ans_vec.size();
-            int cur_last_num=ans_vec.get(as-1);
-            // System.out.println(cur_last_num);
-            if(cur_last_num==1) break;
-            for(int i=0;i<rv.get(cur_last_num).size();++i){
-                int next_num=rv.get(cur_last_num).get(i);
-                if(dp[next_num][ans_cnt-1]+dist[next_num][cur_last_num]==dp[cur_last_num][ans_cnt]){
-                    ans_vec.add(next_num);
-                    ans_cnt-=1;
+        // for(int i=0;i<N;++i){
+        //     for(int j=0;j<M;++j){
+        //         System.out.print(dist[i][j]);
+        //     }System.out.println();
+        // }
+        
+        // 이동하는 거리에서 최솟값이 가장 크도록 할 때 그 값은?
+        int cy=0,cx=0;
+        for(int i=0;i<N;++i){
+            for(int j=0;j<M;++j){
+                if(board[i][j]=='V'){
+                    cy=i;
+                    cx=j;
+                }else{
+                    
+                }
+            }
+        }
+        // System.out.println(cy+" "+cx);
+        int L=0, R=N*M;
+        // System.out.println(R);
+        int min_num=0;
+        boolean visited[][]=new boolean[500+1][500+1];
+        while(L<=R){
+            // System.out.println(L+" "+R);
+            int mid=(L+R)/2;
+            // 최솟값이 mid로 가능한지?
+            boolean flag=false;
+            for(int i=0;i<N;++i){
+                for(int j=0;j<M;++j){
+                    visited[i][j]=false;
+                }
+            }
+            q=new LinkedList<>();
+            q.add(new Pos(cy,cx));
+            visited[cy][cx]=true;
+            while(!q.isEmpty()){
+                Pos p=q.poll();
+                int y=p.y;
+                int x=p.x;
+                // if(L==0 && R==2){
+                //     System.out.println(y+" "+x+" "+dist[y][x]);
+                // }
+                if(dist[y][x]<mid) break;
+                // if(y==3 && x==2){
+                //     System.out.println("fdsfsdf");
+                // }
+                if(board[y][x]=='J'){
+                    flag=true;
                     break;
                 }
+                for(int i=0;i<4;++i){
+                    int ny=y+dy[i];
+                    int nx=x+dx[i];
+                    
+                    if(!isRange(ny,nx)) continue;
+                    if(dist[ny][nx]<mid) continue;
+                    if(visited[ny][nx]) continue;
+                    // if(y==3 && x==2){
+                    //     System.out.println("rkskekfkak"+ny+" "+nx+" "+dist[ny][nx]);
+                    // }
+                    q.add(new Pos(ny,nx));
+                    visited[ny][nx]=true;
+                }
+            }
+            if(flag==true){
+                // System.out.println("fdsfds");
+                min_num=Math.max(min_num,mid);
+                L=mid+1;
+            }else{
+                R=mid-1;
             }
         }
-        Collections.reverse(ans_vec);
-        for(int i=0;i<ans_vec.size();++i){
-            System.out.print(ans_vec.get(i)+" ");
-        }
+        System.out.println(min_num);
     }
 
     public static void main(String[] args) throws IOException{

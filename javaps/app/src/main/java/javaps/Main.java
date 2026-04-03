737fda23af83e102e9c0f1dbbf17b83c3c54e5a1
hyeonseong <sagiri0917@gmail.com>
2026-04-03 22:24:27 +0900

    [BOJ] 24513: 좀비 바이러스

--- javaps/app/src/main/java/javaps/Main.java
+++ javaps/app/src/main/java/javaps/Main.java
@@ -6,48 +6,100 @@ public class Main {
     static Scanner sc=new Scanner(System.in);
     static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    static int N,K;
-    static ArrayList<Integer> v=new ArrayList<>();
+    static int n,m;
+    static int board[][]=new int[1000+1][1000+1];
     public static void Input() throws IOException{
         st = new StringTokenizer(br.readLine());
-        N=Integer.parseInt(st.nextToken());
-        K=Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine());
-        for(int i=0;i<N;++i){
-            int a=Integer.parseInt(st.nextToken());
-            v.add(a);
+        n=Integer.parseInt(st.nextToken());
+        m=Integer.parseInt(st.nextToken());
+        for(int i=0;i<n;++i){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0;j<m;++j){
+                
+                String s=st.nextToken();
+                board[i][j]=Integer.parseInt(s);
+            }
         }
     }
-    
+    static class Pos{
+        int y;
+        int x;
+        
+        public Pos(int y, int x){
+            this.y=y;
+            this.x=x;
+            
+        }
+    }
+    static boolean isRange(int y, int x){
+        return (y>=0 && x>=0 && y<n && x<m);
+    }
+    static int dy[]={-1,0,1,0};
+    static int dx[]={0,1,0,-1};
     public static void Solve(){
-        Collections.sort(v);
-        int idx=-1;
-        for(int i=0;i<N;++i){
-            if(v.get(i)>=K){
-                idx=i;
-                break;
+        Queue<Pos> q=new LinkedList<>();
+        boolean visited[][]=new boolean[1000+1][1000+1];
+        int check[][]=new int[1000+1][1000+1];
+        for(int i=0;i<n;++i){
+            for(int j=0;j<m;++j){
+                Pos p;
+                if(board[i][j]==1){
+                    // visited[i][j]=1;
+                    check[i][j]=1;
+                    p=new Pos(i,j);
+                    q.add(p);
+                }else if(board[i][j]==2){
+                    // visited[i][j]=1;
+                    p=new Pos(i,j);
+                    check[i][j]=1;
+                    q.add(p);
+                }
+                
             }
         }
-        int ans=0;
-        int L=0, R=N-1;
-        if(idx!=-1){
-            ans=N-idx;
-            R=idx-1;
-        }
-        while(L<R){
-            int sum=v.get(L)+v.get(R);
-            if(sum>=K){
-                ++ans;
-                ++L;
-                --R;
-            }else{
-                ++L;
+        while(!q.isEmpty()){
+            int qs=q.size();
+            for(int i=0;i<qs;++i){
+                Pos p=q.poll();
+                int y=p.y;
+                int x=p.x;
+                if(board[y][x]==3) continue;
+                for(int j=0;j<4;++j){
+                    int ny=y+dy[j];
+                    int nx=x+dx[j];
+                    if(!isRange(ny,nx)) continue;
+                    if(board[ny][nx]==board[y][x]) continue;
+                    if(board[ny][nx]==-1) continue;
+                    if(board[ny][nx]==0){
+                        check[ny][nx]=check[y][x]+1;
+                        board[ny][nx]=board[y][x];
+                        q.add(new Pos(ny,nx));
+                    }else{
+                        if(check[ny][nx]==check[y][x]+1){
+                            board[ny][nx]=3;
+                        }
+                    }
+                }
             }
         }
-        if(ans==0){
-            --ans;
+        int fst_cnt=0, sed_cnt=0, trd_cnt=0;
+        // for(int i=0;i<n;++i){
+        //     for(int j=0;j<m;++j){
+        //         System.out.print(board[i][j]);
+        //     }System.out.println();
+        // }
+        for(int i=0;i<n;++i){
+            for(int j=0;j<m;++j){
+                if(board[i][j]==1){
+                    ++fst_cnt;
+                }else if(board[i][j]==2){
+                    ++sed_cnt;
+                }else if(board[i][j]==3){
+                    ++trd_cnt;
+                }
+            }
         }
-        System.out.println(ans);
+        System.out.print(fst_cnt+" "+sed_cnt+" "+trd_cnt);
     }
 
     public static void main(String[] args) throws IOException{

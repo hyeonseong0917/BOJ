85633f7dcb011586c3478f9116c8d54cc45b068b
hyeonseong <sagiri0917@gmail.com>
2026-04-04 22:28:19 +0900

    [BOJ] 30014: 준영이의 사랑

--- javaps/app/src/main/java/javaps/Main.java
+++ javaps/app/src/main/java/javaps/Main.java
@@ -6,86 +6,47 @@ public class Main {
     static Scanner sc=new Scanner(System.in);
     static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    static int N,M,K;
-    static int board[][]=new int[1000+1][1000+1];
-    static int dist[][]=new int[1000+1][1000+1];
-    static int dy[]={-1,0,1,0};
-    static int dx[]={0,1,0,-1};
-    static boolean isRange(int y, int x){
-        return (y>=0 && x>=0 && y<N && x<M);
-    }
-    static class Pos implements Comparable<Pos>{
-        int d;
-        int y;
-        int x;
-        public Pos(int d, int y, int x){
-            this.d=d;
-            this.y=y;
-            this.x=x;
-        }
-        @Override
-        public int compareTo(Pos other){
-            return this.d-other.d;
-        }
-    }
+    static int N;
+    static ArrayList<Integer> v=new ArrayList<>();
+    static int ans[]=new int[2000+1];
     public static void Input() throws IOException{
         st = new StringTokenizer(br.readLine());
         N=Integer.parseInt(st.nextToken());
-        M=Integer.parseInt(st.nextToken());
-        K=Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
         for(int i=0;i<N;++i){
-            st = new StringTokenizer(br.readLine());
-            for(int j=0;j<M;++j){
-                int a=Integer.parseInt(st.nextToken());
-                board[i][j]=a;
-                dist[i][j]=2023101800;
-            }
+            
+            int a=Integer.parseInt(st.nextToken());
+            v.add(a);
         }
+        
     }
     
     public static void Solve(){
-        PriorityQueue<Pos> q=new PriorityQueue<>();
+        Collections.sort(v,Collections.reverseOrder());
+        int L=0;
+        int R=N-1;
+        int idx=0;
+        while(L<=R){
+            if(idx%2==0){
+                ans[L]=v.get(idx);
+                ++L;
+            }else{
+                ans[R]=v.get(idx);
+                --R;
+            }
+            ++idx;
+        }
+        long answer=ans[0]*ans[N-1];
+        for(int i=1;i<N;++i){
+            answer+=ans[i]*ans[i-1];
+        }
+        System.out.println(answer);
         for(int i=0;i<N;++i){
-            q.add(new Pos(board[i][0],i,0));
-            dist[i][0]=board[i][0];
-        }
-        for(int i=0;i<M;++i){
-            q.add(new Pos(board[0][i],0,i));
-            dist[0][i]=board[0][i];
-        }
-        for(int i=0;i<N;++i){
-            q.add(new Pos(board[i][M-1],i,M-1));
-            dist[i][M-1]=board[i][M-1];
-        }
-        while(!q.isEmpty()){
-            Pos p=q.poll();
-            int d=p.d;
-            int y=p.y;
-            int x=p.x;
-            for(int i=0;i<4;++i){
-                int ny=y+dy[i];
-                int nx=x+dx[i];
-                if(!isRange(ny,nx)) continue;
-                if(dist[ny][nx]>dist[y][x]){
-                    dist[ny][nx]=Math.max(board[ny][nx],dist[y][x]);
-                    q.add(new Pos(dist[ny][nx],ny,nx));
-                }
+            System.out.print(ans[i]);
+            if(i!=N-1){
+                System.out.print(" ");
             }
         }
-        // for(int i=0;i<N;++i){
-        //     for(int j=0;j<M;++j){
-        //         System.out.print(dist[i][j]+" ");
-        //     }System.out.println();
-        // }
-        ArrayList<Integer> v=new ArrayList<>();
-        for(int i=0;i<N;++i){
-            for(int j=0;j<M;++j){
-                v.add(dist[i][j]);
-            }
-        }
-        Collections.sort(v);
-        int ans=v.get(K-1);
-        System.out.println(ans);
     }
 
 

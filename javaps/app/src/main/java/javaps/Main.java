a619cbb1e5bf23cf9ee148cdb923f175c1e15a86
hyeonseong <sagiri0917@gmail.com>
2026-04-04 13:26:46 +0900

    [BOJ] 14953: Game Map

--- javaps/app/src/main/java/javaps/Main.java
+++ javaps/app/src/main/java/javaps/Main.java
@@ -6,100 +6,66 @@ public class Main {
     static Scanner sc=new Scanner(System.in);
     static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    static int n,m;
-    static int board[][]=new int[1000+1][1000+1];
+    static int N,M;
+    static ArrayList<ArrayList<Integer>> g=new ArrayList<ArrayList<Integer>>();
+    static int check[]=new int[100000+1];
+    static int dp[]=new int[100000+1];
+    static class CntNum implements Comparable<CntNum>{
+        int cnt;
+        int num;
+        public CntNum(int cnt, int num){
+            this.cnt=cnt;
+            this.num=num;
+        }
+        @Override
+        public int compareTo(CntNum other){
+            return this.cnt-other.cnt;
+        }
+    }
+    static ArrayList<CntNum> v=new ArrayList<>();
     public static void Input() throws IOException{
         st = new StringTokenizer(br.readLine());
-        n=Integer.parseInt(st.nextToken());
-        m=Integer.parseInt(st.nextToken());
-        for(int i=0;i<n;++i){
+        N=Integer.parseInt(st.nextToken());
+        M=Integer.parseInt(st.nextToken());
+        for(int i=0;i<N;++i){
+            g.add(new ArrayList<>());
+        }
+        for(int i=0;i<M;++i){
             st = new StringTokenizer(br.readLine());
-            for(int j=0;j<m;++j){
-                
-                String s=st.nextToken();
-                board[i][j]=Integer.parseInt(s);
-            }
+            int a=Integer.parseInt(st.nextToken());
+            int b=Integer.parseInt(st.nextToken());
+            g.get(a).add(b);
+            g.get(b).add(a);
         }
-    }
-    static class Pos{
-        int y;
-        int x;
-        
-        public Pos(int y, int x){
-            this.y=y;
-            this.x=x;
-            
+        for(int i=0;i<N;++i){
+            int gs=g.get(i).size();
+            check[i]=gs;
+            v.add(new CntNum(gs,i));
+            dp[i]=1;
         }
-    }
-    static boolean isRange(int y, int x){
-        return (y>=0 && x>=0 && y<n && x<m);
-    }
-    static int dy[]={-1,0,1,0};
-    static int dx[]={0,1,0,-1};
-    public static void Solve(){
-        Queue<Pos> q=new LinkedList<>();
-        boolean visited[][]=new boolean[1000+1][1000+1];
-        int check[][]=new int[1000+1][1000+1];
-        for(int i=0;i<n;++i){
-            for(int j=0;j<m;++j){
-                Pos p;
-                if(board[i][j]==1){
-                    // visited[i][j]=1;
-                    check[i][j]=1;
-                    p=new Pos(i,j);
-                    q.add(p);
-                }else if(board[i][j]==2){
-                    // visited[i][j]=1;
-                    p=new Pos(i,j);
-                    check[i][j]=1;
-                    q.add(p);
-                }
-                
-            }
-        }
-        while(!q.isEmpty()){
-            int qs=q.size();
-            for(int i=0;i<qs;++i){
-                Pos p=q.poll();
-                int y=p.y;
-                int x=p.x;
-                if(board[y][x]==3) continue;
-                for(int j=0;j<4;++j){
-                    int ny=y+dy[j];
-                    int nx=x+dx[j];
-                    if(!isRange(ny,nx)) continue;
-                    if(board[ny][nx]==board[y][x]) continue;
-                    if(board[ny][nx]==-1) continue;
-                    if(board[ny][nx]==0){
-                        check[ny][nx]=check[y][x]+1;
-                        board[ny][nx]=board[y][x];
-                        q.add(new Pos(ny,nx));
-                    }else{
-                        if(check[ny][nx]==check[y][x]+1){
-                            board[ny][nx]=3;
-                        }
+        Collections.sort(v);
+        for(int i=0;i<N;++i){
+            int cur_cnt=v.get(i).cnt;
+            int cur_num=v.get(i).num;
+            for(int j=0;j<g.get(cur_num).size();++j){
+                int next_num=g.get(cur_num).get(j);
+                int next_cnt=check[next_num];
+                if(cur_cnt<next_cnt){
+                    if(dp[next_num]<dp[cur_num]+1){
+                        dp[next_num]=dp[cur_num]+1;
                     }
                 }
             }
         }
-        int fst_cnt=0, sed_cnt=0, trd_cnt=0;
-        // for(int i=0;i<n;++i){
-        //     for(int j=0;j<m;++j){
-        //         System.out.print(board[i][j]);
-        //     }System.out.println();
-        // }
-        for(int i=0;i<n;++i){
-            for(int j=0;j<m;++j){
-                if(board[i][j]==1){
-                    ++fst_cnt;
-                }else if(board[i][j]==2){
-                    ++sed_cnt;
-                }else if(board[i][j]==3){
-                    ++trd_cnt;
-                }
-            }
+        int ans=0;
+        for(int i=0;i<N;++i){
+            ans=Math.max(ans,dp[i]);
         }
-        System.out.print(fst_cnt+" "+sed_cnt+" "+trd_cnt);
+        System.out.println(ans);
+    }
+    
+    public static void Solve(){
+            
     }
 
     public static void main(String[] args) throws IOException{

9efea080c41e5834acef6fba7e1baac2d6ca86ed
hyeonseong <sagiri0917@gmail.com>
2026-04-04 15:32:13 +0900

    [BOJ] 15811: 복면?!

--- javaps/app/src/main/java/javaps/Main.java
+++ javaps/app/src/main/java/javaps/Main.java
@@ -6,67 +6,112 @@ public class Main {
     static Scanner sc=new Scanner(System.in);
     static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    static int N,M;
-    static ArrayList<ArrayList<Integer>> g=new ArrayList<ArrayList<Integer>>();
-    static int check[]=new int[100000+1];
-    static int dp[]=new int[100000+1];
-    static class CntNum implements Comparable<CntNum>{
-        int cnt;
-        int num;
-        public CntNum(int cnt, int num){
-            this.cnt=cnt;
-            this.num=num;
-        }
-        @Override
-        public int compareTo(CntNum other){
-            return this.cnt-other.cnt;
-        }
-    }
-    static ArrayList<CntNum> v=new ArrayList<>();
+    static String A,B,C;
+    static int check[]=new int[26];
+    static int visited[]=new int[10];
+    static ArrayList<Integer> tmp=new ArrayList<>();
     public static void Input() throws IOException{
         st = new StringTokenizer(br.readLine());
-        N=Integer.parseInt(st.nextToken());
-        M=Integer.parseInt(st.nextToken());
-        for(int i=0;i<N;++i){
-            g.add(new ArrayList<>());
-        }
-        for(int i=0;i<M;++i){
-            st = new StringTokenizer(br.readLine());
-            int a=Integer.parseInt(st.nextToken());
-            int b=Integer.parseInt(st.nextToken());
-            g.get(a).add(b);
-            g.get(b).add(a);
-        }
-        for(int i=0;i<N;++i){
-            int gs=g.get(i).size();
-            check[i]=gs;
-            v.add(new CntNum(gs,i));
-            dp[i]=1;
-        }
-        Collections.sort(v);
-        for(int i=0;i<N;++i){
-            int cur_cnt=v.get(i).cnt;
-            int cur_num=v.get(i).num;
-            for(int j=0;j<g.get(cur_num).size();++j){
-                int next_num=g.get(cur_num).get(j);
-                int next_cnt=check[next_num];
-                if(cur_cnt<next_cnt){
-                    if(dp[next_num]<dp[cur_num]+1){
-                        dp[next_num]=dp[cur_num]+1;
-                    }
-                }
+        A=st.nextToken();
+        B=st.nextToken();
+        C=st.nextToken();
+    }
+    static ArrayList<Character> v=new ArrayList<>();
+    static int finish=0;
+    static int m[]=new int[26];
+    // v의 i번째 문자는, tmp의 i번째 값과 매칭됨
+    static void dfs(int idx){
+        if(finish==1) return;
+        // tmp의 idx번째 인덱스에 어떤 값을 넣을 것인지??
+        int vs=v.size();
+        if(idx==vs){
+            // 연산 가능한지??
+            int as=A.length();
+            int bs=B.length();
+            int cs=C.length();
+            for(int i=0;i<v.size();++i){
+                char c=v.get(i);
+                m[c-'A']=tmp.get(i);
             }
+            long fs=0, ss=0, ts=0;
+            for(int i=0;i<as;++i){
+                fs*=10;
+                fs+=m[A.charAt(i)-'A'];
+            }
+            for(int i=0;i<bs;++i){
+                ss*=10;
+                ss+=m[B.charAt(i)-'A'];
+            }
+            for(int i=0;i<cs;++i){
+                ts*=10;
+                ts+=m[C.charAt(i)-'A'];
+            }
+            if((fs+ss)==ts){
+                finish=1;
+            }
+            return;
         }
-        int ans=0;
-        for(int i=0;i<N;++i){
-            ans=Math.max(ans,dp[i]);
+        for(int i=0;i<10;++i){
+            if(visited[i]==1) continue;
+            visited[i]=1;
+            tmp.add(i);
+            int ts=tmp.size();
+            dfs(idx+1);
+            tmp.remove(ts-1);
+            visited[i]=0;
         }
-        System.out.println(ans);
     }
     
     public static void Solve(){
-            
+        String fst_tmp=new String("");
+        int as=A.length();
+        for(int i=as-1;i>=0;--i){
+            fst_tmp+=A.charAt(i);
+            ++check[A.charAt(i)-'A'];
+        }
+        // A=new String(fst_tmp);
+        
+        String sed_tmp=new String("");
+        int bs=B.length();
+        for(int i=bs-1;i>=0;--i){
+            sed_tmp+=B.charAt(i);
+            ++check[B.charAt(i)-'A'];
+        }
+        // B=new String(sed_tmp);
+
+        String trd_tmp=new String("");
+        int cs=C.length();
+        for(int i=cs-1;i>=0;--i){
+            trd_tmp+=C.charAt(i);
+            ++check[C.charAt(i)-'A'];
+        }
+        // C=new String(trd_tmp);
+        int cnt=0;
+        for(int i=0;i<26;++i){
+            if(check[i]>0){
+                ++cnt;
+            }
+        }
+        if(cnt>10){
+            System.out.print("NO");
+            return;
+        }
+        for(int i=0;i<26;++i){
+            if(check[i]>0){
+                char c='A';
+                c+=i;
+                v.add(c);
+            }
+        }
+        // v에 있는 수들을 0~9 중에서 분배하기
+        dfs(0);
+        if(finish==1){
+            System.out.print("YES");
+        }else{
+            System.out.print("NO");
+        }
     }
+
 
     public static void main(String[] args) throws IOException{
         Input();
